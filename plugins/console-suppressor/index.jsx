5b33a43e1af3f167a4f92a4b7ca844311c69c92c
Milkshift <99212308+Milkshiift@users.noreply.github.com>
2024-06-29 15:12:16 +0300

    Add console suppressor

---
+++ plugins/console-suppressor/index.jsx
@@ -0,0 +1,33 @@
+let originalConsoleMethods = {};
+
+function filterConsoleMessages() {
+    // Iterate through all console methods and filter messages
+    Object.keys(console).forEach(method => {
+        originalConsoleMethods[method] = console[method];
+
+        console[method] = function () {
+            const message = arguments[0];
+            if (message.includes("%c[")) {
+                return;
+            }
+            originalConsoleMethods[method].apply(console, arguments);
+        };
+    });
+}
+
+function revertConsoleMessages() {
+    // Restore original console methods
+    Object.keys(originalConsoleMethods).forEach(method => {
+        console[method] = originalConsoleMethods[method];
+    });
+    // Clear the originalConsoleMethods object
+    originalConsoleMethods = {};
+}
+
+export async function onLoad() {
+    filterConsoleMessages();
+}
+
+export function onUnload() {
+    revertConsoleMessages();
+}
