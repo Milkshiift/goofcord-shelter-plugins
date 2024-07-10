3782ee0ca674d0697fb9fea35bbfbad88dd44ef3
Milkshift <99212308+Milkshiift@users.noreply.github.com>
2024-07-10 23:30:47 +0300

    Improve settings-category

--- plugins/settings-category/index.jsx
+++ plugins/settings-category/index.jsx
@@ -6,9 +6,10 @@ const {
 
 const unregisters = [];
 export async function onLoad() {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     unregisters.push(registerSection("divider"));
     unregisters.push(registerSection("header", "GoofCord"));
-    unregisters.push(registerSection("section", "goofcord", "Settings", () => window.goofcord.openSettingsWindow));
+    unregisters.push(registerSection("button", "goofcord", "Settings", ()=>{window.goofcord.openSettingsWindow()}));
 }
 
 export function onUnload() {
